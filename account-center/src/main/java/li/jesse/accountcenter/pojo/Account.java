d4321b46075aec86cf84b4975b6cc3179505e7a4
Junying Li <jesselix@live.com>
2019-11-28 00:05:19 +0800

    +

---
+++ account-center/src/main/java/li/jesse/accountcenter/pojo/Account.java
@@ -0,0 +1,21 @@
+package li.jesse.accountcenter.pojo;
+
+import lombok.Data;
+
+@Data
+public class Account {
+
+    private String accountId;
+
+    private String surname;
+
+    private String givenName;
+
+    private String middleName;
+
+    private String primaryMobile;
+
+    private String primaryEmail;
+
+
+}

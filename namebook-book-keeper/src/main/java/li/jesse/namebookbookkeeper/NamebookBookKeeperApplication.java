6b08fd78613e63fc1788941bd1a368ef7c707bab
Junying Li <jesselix@live.com>
2020-02-13 19:44:19 +0800

    +

---
+++ namebook-book-keeper/src/main/java/li/jesse/namebookbookkeeper/NamebookBookKeeperApplication.java
@@ -0,0 +1,13 @@
+package li.jesse.namebookbookkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamebookBookKeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NamebookBookKeeperApplication.class, args);
+    }
+
+}

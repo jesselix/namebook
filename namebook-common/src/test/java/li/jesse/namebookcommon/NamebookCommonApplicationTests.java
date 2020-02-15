16b642957c94a520640c9a8300aa160171fe214f
Junying Li <jesselix@live.com>
2020-02-15 19:56:23 +0800

    +

---
+++ namebook-common/src/test/java/li/jesse/namebookcommon/NamebookCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package li.jesse.namebookcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamebookCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

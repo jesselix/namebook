b55ee8be7e02f422d46173816898de021f91a0a6
Junying Li <jesselix@live.com>
2020-03-17 20:31:10 +0800

    +

--- namebook-account/src/main/java/li/jesse/namebookaccount/config/SwaggerConfig.java
+++ namebook-account/src/main/java/li/jesse/namebookaccount/config/SwaggerConfig.java
@@ -34,7 +34,7 @@ public class SwaggerConfig {
                 "Api Documentation",
                 "1.0",
                 "urn:tos",
-                new Contact("", "", ""),
+                new Contact("Jesse", "", "jesselix@live.com"),
                 "Apache 2.0",
                 "http://www.apache.org/licenses/LICENSE-2.0",
                 new ArrayList<VendorExtension>());

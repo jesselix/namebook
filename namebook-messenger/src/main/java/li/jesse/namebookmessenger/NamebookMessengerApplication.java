14230cb924ee6015a30ec0a81e07870b060b41ab
Junying Li <jesselix@live.com>
2020-04-04 21:56:51 +0800

    +

---
+++ namebook-messenger/src/main/java/li/jesse/namebookmessenger/NamebookMessengerApplication.java
@@ -0,0 +1,13 @@
+package li.jesse.namebookmessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamebookMessengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NamebookMessengerApplication.class, args);
+    }
+
+}

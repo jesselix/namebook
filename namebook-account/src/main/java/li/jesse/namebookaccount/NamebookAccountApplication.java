72fc8cbc42d76571f481c5c64f41d32aa085a90a
Junying Li <jesselix@live.com>
2020-07-12 17:15:16 +0800

    +

--- namebook-account/src/main/java/li/jesse/namebookaccount/NamebookAccountApplication.java
+++ namebook-account/src/main/java/li/jesse/namebookaccount/NamebookAccountApplication.java
@@ -1,6 +1,5 @@
 package li.jesse.namebookaccount;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,4 +9,5 @@ public class NamebookAccountApplication {
     public static void main(String[] args) {
         SpringApplication.run(NamebookAccountApplication.class, args);
     }
+
 }
---
+++ sql/namebook-account_drop_delete.sql
@@ -0,0 +1,7 @@
+use namebook_account;
+
+# drop
+drop table person_name;
+
+# delete
+delete from person_name;

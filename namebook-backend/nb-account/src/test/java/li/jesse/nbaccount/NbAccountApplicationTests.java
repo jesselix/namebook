46493aae2ee3787e0c24fe72dc41ba547d8d51ab
Junying Li <jesselix@live.com>
2021-06-01 11:56:43 +0800

    +

---
+++ namebook-backend/nb-account/src/test/java/li/jesse/nbaccount/NbAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package li.jesse.nbaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ namebook-backend/src/main/java/li/jesse/namebookbackend/NamebookBackendApplication.java
@@ -0,0 +1,13 @@
+package li.jesse.namebookbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamebookBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NamebookBackendApplication.class, args);
+    }
+
+}
